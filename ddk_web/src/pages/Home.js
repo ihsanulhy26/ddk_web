f263480b1766dc1dd1b109590114562aad9d5a26
ihsanulhy26 <ihsanhadiyunas1@gmail.com>
2024-09-11 16:29:58 +0700

    commit basic

---
+++ ddk_web/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to DDK WEB Dashboard</h1>
+      <p>This is the main dashboard for monitoring nutrunner data in real-time.</p>
+    </div>
+  );
+}
+
+export default Home;
